34dc0c929c4e4eb5c4cd72fbacd574b22cbd6404
VANSH AGARWAL <vanshagarwal11@gmail.com>
2025-01-26 15:25:35 +0530

    Init frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "./components/ui/button";
+
+export default function App(){
+  return(
+    <div className="w-screen h-screen bg-popover-foreground flex items-center justify-center text-6xl font-bold  flex-col gap-10">
+      hare krishna
+      <Button>CLick here</Button>
+    </div>
+  )
+}
